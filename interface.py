5b86dd0a90b9c8abedd84443ac2469c37399b627
Halim-Mouaziz <89255951+Halim-Mouaziz@users.noreply.github.com>
2022-10-15 20:59:42 +0100

    Code Restart

--- interface.py
+++ interface.py
@@ -1,7 +1,5 @@
 from InquirerPy import inquirer
 from InquirerPy.base.control import Choice
-import operations
-from functions import mk_list_from
 import sys
 
 
@@ -17,38 +15,8 @@ def main_menu():
         default=None,
     ).execute()
     if action == "Command Runner":
-        command_runner_ui()
+        pass
     elif action is None:
         print("Exiting...")
         sys.exit(1)
-
-
-def command_runner_ui():
-    action = inquirer.select(
-        message="Command Runner",
-        choices=[
-            Choice(value=1, name="Run any command."),
-            Choice(value=None, name="Return"),
-        ],
-        default=None,
-    ).execute()
-    if action == 1:
-        run_fuzzy_command_ui()
-
-    elif action is None:
-        main_menu()
-
-
-def run_fuzzy_command_ui():
-    command_list = mk_list_from("command_operation_name.csv")
-
-    action = inquirer.fuzzy(
-        message="Run:",
-        choices=command_list,
-        default="",
-    ).execute()
-    if action is not None:
-        operation = getattr(operations, action)
-        operation()
-    command_runner_ui()
 
